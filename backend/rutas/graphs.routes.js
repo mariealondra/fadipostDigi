e2fffee3af4e38f40af3c9bd38a480144fe9591e
mariealondra <97216728+mariealondra@users.noreply.github.com>
2022-08-03 13:07:27 -0400

    Delete backend directory

--- backend/rutas/graphs.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const { getGraphs, getGraphsById, createGraphs, updateGraph, deleteGraphs } = require('../controllers/therapist.controller');
-router.get('/graphs', getGraphs);
-router.get('/graphs/:id', getGraphsById);
-router.post('/graphs', createGraphs);
-router.put('/graphs/:id', updateGraph);
-router.delete('/graphs/:id', deleteGraphs);
-module.exports = router;
